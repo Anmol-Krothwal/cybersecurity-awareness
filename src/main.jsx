2b82e0ccd31c60de4edda7fd004569efa5e6c890
ANMOL KROTHWAL <anmolkrothwal177@gmail.com>
2025-08-15 01:00:18 +0100

    Fix case-sensitive import paths for Linux build

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { store } from './Store.js';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from './Components/ThemeContext';
+import { ThemeProvider } from './Components/Themecontext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
